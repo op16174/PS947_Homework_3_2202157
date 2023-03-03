b4b4157ef6309f0fd65ace2cfe6e56b8af6533e2
op16174 <op16174@essex.ac.uk>
2023-03-03 00:37:24 +0000

    Attempting Question 3 (this one is a big nope too)

--- Homework 3 - R Script - 2202157.R
+++ Homework 3 - R Script - 2202157.R
@@ -27,7 +27,17 @@ str(d)
 #Part 2.3 - Distinction material: The authors of the study wondered whether one explanation for the results could be that participants perceive recommendations in one modality as being more competent, intelligent and/or thoughtful than in the other. Can you find any evidence for this? Does it affect your conclusions about the effect of modality?
 d$Stimulus <- factor(d$Stimulus)
 
+#no clue about how to start Q2 skip 
+
 #QUESTION 3 - SOCIAL JUDGEMENTS OF FACES
 #Part 3.1 - Replicate PCA using ′faces.csv′.
+d2 <- read.csv("faces.csv") 
+summary(d2)
+str(d2)
+
 #Part 3.2 - Testing two factor model on ′faces_big.csv′
+d3 <- read.csv("faces_big.csv") 
+summary(d3)
+str(d3)
+
 #Part 3.3 - Run model, Conclusions about hypothesis
